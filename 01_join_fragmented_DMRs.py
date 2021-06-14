ce73215789d3499d26765f49762be817573b691a
Dario Galanti <58292612+Dario-Galanti@users.noreply.github.com>
2021-06-14 19:41:46 +0200

    Add files via upload

---
+++ 01_join_fragmented_DMRs.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+## Author: Dario Galanti, Feb 2021
+## Input: DMR pipe output file
+## Aim: Merge DMRs closer then "x" bp that have the same directionality (both hyper in the same group)
+## Output: bed file with same format as input but some DMRs merged
+## Run: python3 01_join_fragmented_DMRs.py input.bed output.bed
+## Run: for f in pop_DMRs/CpG/R*.bed;do fout=$(echo $f | rev | cut -c5- | rev)_joined_146bp.bed; python3 01_join_fragmented_DMRs.py $f $fout;done
+
+## Purpose: Especially in CHH (higher density) DMRs are sometimes detected in smaller chuncks instead of detecting full DMRs
+## IMPORTANT: Bare in mind that the DMR stats from this script are not very accurate because they don't make use of the original unionbed files!!!
+## Cs is only obtaines summing up Cs from merged DMRs but does not count any Cs in the region between the merged DMRs
+## The average difference suffers from the same imprecision.
+## The merged_DMR q-value is taken from the original DMR with the minimum q-value
+
+## Import modules
+import sys
+
+## Define input, output and min dist to merge DMRs
+fin = str(sys.argv[1])
+fout = str(sys.argv[2])
+index = "/scr/epi/genomes/thlaspi_arvense/thlaspi.fa.fai"
+min_dist = 146												## IMPORTANT!!! Defines the minimum distance to merge DMRs with same directionality
+
+
+line_num = 0
+with open(fin, "r") as fin, open(fout, "w") as fout:
+	for line in fin:
+		line = line.strip()
+		line = line.split("\t")
+		line_num += 1
+	## Skip first line
+		if (line_num > 1):
+		## Calculate distance from previous DMR and directionality of new DMR
+			dist = (int(line[1]) - end)
+			new_direction = ("-" if float(line[4]) < 0 else "+")
+		## NEW LINE SHOULD BE MERGED TO PREVIOS BLOCK!!! Update few info
+			if (line[0] == scaff) and (dist < min_dist) and (direction == new_direction):
+				Cs += int(line[3])
+				diff += float(line[4]) * int(line[3])
+				sig = min(sig, float(line[5]))
+		## NEW LINE SHOULD BE SEPARATED FROM PREVIOUS BLOCK!!! Print previous block and then update info
+			else:
+			## First print previous block
+				diff = (diff/Cs)
+				length = (end - st)
+				print(scaff,st,end,Cs,diff,sig,length, sep="\t", file=fout)
+			## Then update start and other info
+				scaff = line[0]
+				st = int(line[1])
+				Cs = int(line[3])
+				diff = float(line[4]) * int(line[3])
+				direction = ("-" if float(line[4]) < 0 else "+")
+				sig = float(line[5])
+				
+	## First line: only store DMR info
+		else:
+			scaff = line[0]
+			st = int(line[1])
+			Cs = int(line[3])
+			diff = float(line[4]) * int(line[3])
+			direction = ("-" if float(line[4]) < 0 else "+")
+			sig = float(line[5])
+		## The original DMR end is the only info that has to be updated every line!!!
+		end = int(line[2])
+	
+
+
---
+++ 02_merge_DMRs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Author: Dario Galanti 2020 (modified from Cristian Peña https://github.com/EpiDiverse/scripts/blob/master/merge_DMRs.sh)
+## Aim: This script merges all bed files (included in an input directory) in one unique bed file. In this way each context can be summarized.
+## Input directory: run script in DMR pipe output directory (outdir/${context}/${comparison}/ )
+## Run: bash 02_merge_DMRs.sh context outputDirectory
+## Run: bash 02_merge_DMRs.sh CpG merged_DMRs
+## Dependencies: bedtools (https://bedtools.readthedocs.io/en/latest/content/bedtools-suite.html)
+
+
+cont=$1
+output=$2/$1
+mkdir -p ${output}
+FILES=$(ls ${cont}/*.bed | tr "\n" " ")
+NAMES=$(ls ${cont}/*.bed | cut -f2 -d/ | rev | cut -f2- -d . | rev | tr "\n" " ")
+
+cat ${cont}/*.bed > ${output}/DMRs.bed
+bedtools sort -i ${output}/DMRs.bed > ${output}/DMRs_sorted.bed
+bedtools merge -i ${output}/DMRs_sorted.bed -c 4 -o count > ${output}/DMRs_${cont}_merged.bed
+echo $(wc -l ${output}/DMRs_${cont}_merged.bed) merged DMRs created
+
+rm ${output}/DMRs.bed
+rm ${output}/DMRs_sorted.bed
